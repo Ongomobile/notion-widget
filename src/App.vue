d031c0c9e0a5407d4f8666cfc4fde4796b921769
ongomobile <ongomobile@gmail.com>
2022-01-04 19:12:53 -0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <AddCounters />
+  </div>
+</template>
+
+<script>
+import AddCounters from "./components/AddCounters.vue";
+export default {
+  name: "App",
+  components: {
+    AddCounters,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600;700&display=swap");
+/* Global Styles */
+/* CSS Reset */
+/*
+  1. Use a more-intuitive box-sizing model.
+*/
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+/*
+  2. Remove default margin
+*/
+* {
+  margin: 0;
+}
+/*
+  3. Allow percentage-based heights in the application
+*/
+html,
+body {
+  height: 100%;
+}
+/*
+  Typographic tweaks!
+  4. Add accessible line-height
+  5. Improve text rendering
+*/
+body {
+  touch-action: manipulation;
+  font-family: "Inter", sans-serif;
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+}
+/*
+  6. Improve media defaults
+*/
+img,
+picture,
+video,
+canvas,
+svg {
+  display: block;
+  max-width: 100%;
+}
+/*
+  7. Remove built-in form typography styles
+*/
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+/*
+  8. Avoid text overflows
+*/
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+}
+/*
+  9. Create a root stacking context
+*/
+#root {
+  isolation: isolate;
+}
+/* End reset */
+:root {
+  --colored-text: #36454f;
+  --box-shadow: 0 2px 2px #ccc;
+}
+
+.container {
+  max-width: 1128px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/AddCounters.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="add-counter-form--wrapper">
+    <h2 class="add-counter-heading">Add A Counter</h2>
+    <!-- prevent modifier prevents default -->
+    <form class="add-counter-form" @submit.prevent="addCounter">
+      <label for="type" class="add-counter-label" title="add"
+        >Counter Name</label
+      >
+      <input
+        type="text"
+        id="type"
+        name="type"
+        class="add-counter-input"
+        placeholder="Add Counter Name"
+        v-model="windowType"
+      />
+
+      <label for="price" class="add-counter-label">Counter Price</label>
+      <!-- Use .number modifier to cast input value as number -->
+      <input
+        type="text"
+        id="price"
+        name="price"
+        class="add-counter-input"
+        v-model.number="windowPrice"
+        placeholder="Add Window Price"
+      />
+      <input
+        type="submit"
+        class="add-counter-input submit"
+        value="Add Counter"
+        :disabled="isDisabled"
+      />
+    </form>
+    <div class="total-price-wrapper">
+      <p class="total-text">Total Price {{ grandTotal || 0.0 }}</p>
+      <p>"Feature to be implemented"</p>
+    </div>
+  </div>
+  <div class="counters-wrapper">
+    <ul class="counters-list">
+      <li
+        class="counter-item"
+        v-for="(counter, index) in counters"
+        :key="index"
+      >
+        <Counter :counter="counter" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Counter from "./Counter.vue";
+export default {
+  data() {
+    return {
+      counters: [],
+      grandTotal: 0.0,
+      windowPrice: "",
+      windowType: "",
+      quantity: 0,
+      counterId: 0,
+      subtotal: 0,
+    };
+  },
+  components: {
+    Counter,
+  },
+  methods: {
+    addCounter() {
+      let newCounter = {
+        id: this.counterId + 1,
+        windowType: this.windowType,
+        windowPrice: this.windowPrice,
+        quantity: this.quantity,
+        subtotal: this.subtotal,
+      };
+
+      if (this.windowPrice !== "" && this.windowType !== "") {
+        this.counters.push(newCounter);
+      }
+
+      this.windowPrice = "";
+      this.windowType = "";
+      this.quantity = 0;
+
+      this.counterId = newCounter.id;
+      let countersArr = this.counters;
+      console.log({ countersArr });
+    },
+  },
+  computed: {
+    isDisabled: function () {
+      if (this.windowType !== "" && typeof this.windowPrice === "number") {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.add-counter-form--wrapper {
+  display: flex;
+  flex-direction: column;
+  max-width: 320px;
+  margin: 50px auto 30px;
+}
+
+.add-counter-form {
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  border: solid 1px #eee;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  box-shadow: var(--box-shadow);
+}
+
+.add-counter-heading {
+  margin-bottom: 5px;
+  text-align: center;
+}
+
+.add-counter-input {
+  width: 100%;
+  padding-left: 10px;
+  border-radius: 5px;
+  border: 1px solid #dcdcdc;
+  margin-bottom: 10px;
+}
+
+.add-counter-label {
+  color: #36454f;
+  margin-left: 5px;
+}
+
+.total-price-wrapper {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  box-shadow: var(--box-shadow);
+  border-radius: 5px;
+}
+/* .counters-wrapper {
+  padding: 10px;
+} */
+
+.counters-list {
+  padding: 10px;
+  display: flex;
+  list-style: none;
+  overflow: auto;
+  overflow-x: scroll;
+}
+
+@media screen and (max-width: 340px) {
+  .add-counter-form--wrapper {
+    max-width: 295px;
+  }
+}
+
+/* .counter-item{
+  no styles yet
+} */
+</style>
